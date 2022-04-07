796f503715e8d562de9d0379c741fe960d06311c
Bipperty <ignacio.gibbs@gmail.com>
2022-04-07 19:53:40 -0300

    Multer, photoHandler & ModelPhoto

--- models/server.js
+++ models/server.js
@@ -24,16 +24,16 @@ class Server {
   middlewares() {
     //Configuracion CORS
 
-    this.app.use(cors());
+    this.app.use(cors({ origin: allowedOrigins }));
 
     //Morgan
     this.app.use(logger("dev"));
 
     //lectura de datos enviados en el body  de la peticion
-    this.app.use(express.json());
+    this.app.use(express.json({ extend: true }));
 
     //Configuracion del directorio publico
-    this.app.use(express.static("public"));
+    this.app.use("/uploads", express.static(path.resolve("uploads"))); // Habilito ruta publica
   }
 
   //definimos las rutas
