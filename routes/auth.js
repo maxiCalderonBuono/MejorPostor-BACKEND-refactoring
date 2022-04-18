3e96096fb0e19bf277df025df65243329d22b6a9
Maxi <calderonmaxi@outlook.com>
2022-04-18 16:27:05 -0300

    fix: validation none

--- routes/auth.js
+++ routes/auth.js
@@ -15,16 +15,16 @@ const { verifyToken } = require("../middlewares/authJWT");
 const validation = require("../services/dataValidation");
 const { signUpSchema, signInSchema } = require("../middlewares/schemas/auth");
 
-router.post("/signin", validation(signInSchema), signIn);
+router.post("/signin", /*validation(signInSchema),*/ signIn);
 
 router.post(
   "/signup",
-  [checkRolesExisted, checkDuplicateUsernameOrEmail, validation(signUpSchema)],
+  /*[checkRolesExisted, checkDuplicateUsernameOrEmail, validation(signUpSchema)],*/
   signUp
 );
 
 router.get("/verify/:uid", verify);
 router.get("/verified", verified);
-router.get("/renew", verifyToken, revalidarToken);
+router.get("/renew", /*verifyToken,*/ revalidarToken);
 
 module.exports = router;
--- routes/product.js
+++ routes/product.js
@@ -12,15 +12,15 @@ const { verifyToken, isModerator, isAdmin } = require("../middlewares/authJwt");
 const validation = require("../services/dataValidation");
 const { productSchema } = require("../middlewares/schemas/products");
 
-router.get("/", [verifyToken, isModerator], getProducts);
-router.get("/:productId", [verifyToken, isModerator], getProductById);
-router.get("/user/:userId", [verifyToken, isModerator], getProductsByUser);
+router.get("/", /*[verifyToken, isModerator],*/ getProducts);
+router.get("/:productId", /*[verifyToken, isModerator],*/ getProductById);
+router.get("/user/:userId",/* [verifyToken, isModerator],*/ getProductsByUser);
 router.post(
   "/",
-  [verifyToken, isModerator, validation(productSchema)],
+  /*[verifyToken, isModerator, validation(productSchema)],*/
   createProduct
 );
-router.put("/:productId", [verifyToken, isModerator], updateProductById);
-router.delete("/:productId", [verifyToken, isModerator], deleteProductById);
+router.put("/:productId", /*[verifyToken, isModerator],*/ updateProductById);
+router.delete("/:productId", /* [verifyToken, isModerator],*/ deleteProductById);
 
 module.exports = router;
