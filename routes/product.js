67d84bbf5c67b9b624cbf78d7bf37ea194351e0e
Bipperty <ignacio.gibbs@gmail.com>
2022-04-22 17:15:44 -0300

    e

--- routes/product.js
+++ routes/product.js
@@ -8,15 +8,15 @@ const {
   deleteProductById,
   getProductsByUser,
 } = require("../controllers/productsController");
-//const { verifyToken } = require("../middlewares/authJwt");
+const { verifyToken } = require("../middlewares/authJwt");
 const validation = require("../services/dataValidation");
 const { productSchema } = require("../middlewares/schemas/products");
 
 router.get("/", getProducts);
 router.get("/:productId", getProductById);
-router.get("/user/:userId" /* verifyToken*/, getProductsByUser);
-router.post("/" /* verifyToken*/, validation(productSchema), createProduct);
-router.put("/:productId" /* verifyToken*/, updateProductById);
-router.delete("/:productId" /* verifyToken*/, deleteProductById);
+router.get("/user/:userId", verifyToken, getProductsByUser);
+router.post("/", verifyToken, validation(productSchema), createProduct);
+router.put("/:productId", verifyToken, updateProductById);
+router.delete("/:productId", verifyToken, deleteProductById);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,17 +6,13 @@ const {
   updateUserById,
   deleteUserById,
 } = require("../controllers/usersController.js");
-//const { verifyToken } = require("../middlewares/authJwt");
+const { verifyToken } = require("../middlewares/authJwt");
 const validation = require("../services/dataValidation");
 const { usersSchema } = require("../middlewares/schemas/users");
 
-router.get("/" /* verifyToken*/, getUsers);
-router.get("/:userId" /* verifyToken*/, getUserById);
-router.put(
-  "/:userId",
-  [/* verifyToken*/ validation(usersSchema)],
-  updateUserById
-);
-router.delete("/:userId" /* verifyToken*/, deleteUserById);
+router.get("/", verifyToken, getUsers);
+router.get("/:userId", verifyToken, getUserById);
+router.put("/:userId", [verifyToken, validation(usersSchema)], updateUserById);
+router.delete("/:userId", verifyToken, deleteUserById);
 
 module.exports = router;
